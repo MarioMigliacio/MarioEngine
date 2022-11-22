9002751ba25d99278f025f27e585b0e3e89cc5dc
Mario <migliacio@gmail.com>
2022-11-22 11:45:25 -0800

    Very important project setup.

---
+++ MarioEngine/src/test.cpp
@@ -0,0 +1,11 @@
+#include "test.h"
+#include <stdio.h>
+
+namespace ME {
+
+    void Print()
+    {
+        printf("Mario Engine!\n");
+    }
+
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,11 @@
+
+namespace ME {
+
+    __declspec(dllimport) void Print();
+
+}
+
+void main()
+{
+    ME::Print();
+}
