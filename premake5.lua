afc57337a61557911ab985f5641847dbfa07c7b6
Mario <migliacio@gmail.com>
2022-11-23 10:29:48 -0800

    Finally got premake to work with solution

--- premake5.lua
+++ premake5.lua
@@ -1,104 +1,106 @@
 workspace "MarioEngine"
-    architecture "x64"
-    startproject "Sandbox"
+	architecture "x64"
+	startproject "Sandbox"
 
-    configurations
-    {
-        "Debug",
-        "Release",
-        "Dist"
-    }
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
 
-    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-    
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
 project "MarioEngine"
-    location "MarioEngine"
-    kind "SharedLib"
-    language "C++"
+	location "MarioEngine"
+	kind "SharedLib"
+	language "C++"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "${prj.name}/src/**.h",
-        "${prj.name}/src/**.cpp"
-    }
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-    includedirs
-    {
-        "%{prj.name}/vendor/spdlog/include"
-    }
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
 
-    filter "system:windows"
-        cppdialect "C++17"
-        staticruntime "On"
-        systemversion "10.0.22621.0"
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
 
-        defines
-        {
-            "MARIOENGINE_PLATFORM_WINDOWS",
-            "MARIOENGINE_BUILD_DLL"
-        }
+		defines
+		{
+			"MARIOENGINE_PLATFORM_WINDOWS",
+			"MARIOENGINE_BUILD_DLL"
+		}
 
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
 
-    filter "configurations:Debug"
-        defines "MARIOENGINE_DEBUG"
-        symbols "On"
+	filter "configurations:Debug"
+		defines "MARIOENGINE_DEBUG"
+		symbols "On"
 
-    filter "configurations:Release"
-        defines "MARIOENGINE_RELEASE"
-        optimize "On"
+	filter "configurations:Release"
+		defines "MARIOENGINE_RELEASE"
+		optimize "On"
 
-    filter "configurations:Dist"
-        defines "MARIOENGINE_DIST"
-        optimize "On"
+	filter "configurations:Dist"
+		defines "MARIOENGINE_DIST"
+		optimize "On"
 
 project "Sandbox"
-    location "Sandbox"
-    kind "ConsoleApp"
-    language "C++"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "${prj.name}/src/**.h",
-        "${prj.name}/src/**.cpp"
-    }
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-    includedirs
-    {
-        "MarioEngine/vendor/spdlog/include",
-        "MarioEngine/src"
-    }
+	includedirs
+	{
+		"MarioEngine/vendor/spdlog/include",
+		"MarioEngine/src"
+	}
 
-    links
-    {
-        "MarioEngine"
-    }
+	links
+	{
+		"MarioEngine"
+	}
 
-    filter "system:windows"
-        cppdialect "C++17"
-        staticruntime "On"
-        systemversion "10.0.22621.0"
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
 
-        defines
-        {
-            "MARIOENGINE_PLATFORM_WINDOWS"
-        }
+		defines
+		{
+			"MARIOENGINE_PLATFORM_WINDOWS"
+		}
 
+	filter "configurations:Debug"
+		defines "MARIOENGINE_DEBUG"
+		symbols "On"
 
-    filter "configurations:Debug"
-        defines "MARIOENGINE_DEBUG"
-        symbols "On"
+	filter "configurations:Release"
+		defines "MARIOENGINE_RELEASE"
+		optimize "On"
 
-    filter "configurations:Release"
-        defines "MARIOENGINE_RELEASE"
-        optimize "On"
-
-    filter "configurations:Dist"
-        defines "MARIOENGINE_DIST"
-        optimize "On"
-
+	filter "configurations:Dist"
+		defines "MARIOENGINE_DIST"
+		optimize "On"
