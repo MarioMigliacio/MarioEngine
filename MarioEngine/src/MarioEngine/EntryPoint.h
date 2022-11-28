9836c73f99dafea758490a3d7d240feb2cd18593
Mario <migliacio@gmail.com>
2022-11-28 12:09:45 -0800

    Regenerated project files, only changed main slightly.

--- MarioEngine/src/MarioEngine/EntryPoint.h
+++ MarioEngine/src/MarioEngine/EntryPoint.h
@@ -8,8 +8,6 @@ int main(int argc, char** argv)
 {
     MarioEngine::Log::Init();
     MARIOENGINE_CORE_WARN("Mario Engine up and running!");
-    int num = 10;
-    MARIOENGINE_CLIENT_INFO("Hello mario app! Variable: {0}", num);
 
     auto app = MarioEngine::CreateApplication();
     app->Run();
